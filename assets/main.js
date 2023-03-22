2b341fceae65a672a6bd9366b6b9a80af92a932a
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-22 15:54:32 +0100

    svolgimento della funzione

---
+++ assets/main.js
@@ -0,0 +1,49 @@
+/*
+- Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+    -input/prompt richiesta km ed eta
+- Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    - esempio 20 * 20 =
+- va applicato uno sconto del 20% per i minorenni
+    -condizione if su eta, poi
+    - prezzo * 0.2
+- va applicato uno sconto del 40% per gli over 65.
+    -condizione if su eta, poi
+    - prezzo * 0.4
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+
+
+// METODO 1
+
+
+function prezzo(){
+
+    let chilometri = document.getElementById(`chilometri`).value
+    let eta = document.getElementById(`eta`).value
+    const prezzoAlChilometro = 0.21
+    let prezzoBigliettoIntero = `${prezzoAlChilometro * chilometri}`
+    let prezzo20percento = `${prezzoBigliettoIntero * 0.20}`
+    let prezzo40percento = `${prezzoBigliettoIntero * 0.40}`
+
+    console.log(`${prezzoBigliettoIntero}`)
+
+    if(eta <= 17){
+        console.log(`il prezzo diventa:${prezzoBigliettoIntero - prezzo20percento}`)
+    }else if(eta >= 18 || eta < 65){
+        console.log(`${prezzoBigliettoIntero}`)
+    }
+    
+    if(eta >= 65){
+        console.log(`il prezzo diventa:${prezzoBigliettoIntero - prezzo40percento}`)
+    }
+
+
+    // console.log.apply(document.getElementById(`prezzoformaumana`).innerHTML = `${prezzo}`)
+}
+
+// console.log.apply(document.getElementById(`prezzoformaumana`).innerHTML = `${prezzo}`)
+
+
+
